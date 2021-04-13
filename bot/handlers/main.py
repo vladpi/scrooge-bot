fda43419bb982fd32d8b14e8143954aabebbd39b
Vlad Pastushenko <vladp@ro.ru>
2021-04-14 00:49:01 +0300

    fix: hide history and reports for empty transactions

--- bot/handlers/main.py
+++ bot/handlers/main.py
@@ -1,10 +1,17 @@
+from typing import TYPE_CHECKING
+
 from aiogram import types
 from aiogram.dispatcher import FSMContext, filters
 
 from bot import dispatcher, views
+from modules.transactions import count_transactions_by_user
+
+if TYPE_CHECKING:
+    from modules.users import UserSchema
 
 
 @dispatcher.message_handler(filters.CommandStart(), state='*')
-async def start(message: types.Message, state: FSMContext):
+async def start(message: types.Message, state: FSMContext, user: 'UserSchema'):
+    has_transactions = await count_transactions_by_user(user.id) > 0
     await state.reset_state()
-    await views.main.main_menu(message.chat.id)
+    await views.main.main_menu(message.chat.id, has_transactions=has_transactions)
--- bot/views/main.py
+++ bot/views/main.py
@@ -6,13 +6,17 @@ from bot import bot
 from bot.resources import buttons, messages
 
 
-async def main_menu(to_chat_id: int, text: Optional[str] = None):
+async def main_menu(
+    to_chat_id: int, text: Optional[str] = None, has_transactions: bool = True,
+):
     if text is None:
         text = messages.MAIN_MENU
 
     keyboard = [
         [KeyboardButton(buttons.ADD_EXPENSE)],
-        [KeyboardButton(buttons.HISTORY), KeyboardButton(buttons.REPORTS)],
+        [KeyboardButton(buttons.HISTORY), KeyboardButton(buttons.REPORTS)]
+        if has_transactions
+        else [],
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
